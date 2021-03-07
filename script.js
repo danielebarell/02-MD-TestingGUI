2f8ac207fac6e2e2509a8b4c5c4f1c4b7026841e
danielebarell <dan.barell@gmmail.com>
2021-03-07 13:01:38 +0100

    page layout init and logic

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import {PerfectSquare} from "./js/perfect-square.js";
+import {PageLayout} from "./js/page-layout.js";
+
 const perfectSquare = new PerfectSquare(document.querySelector('#shape-container'));
+const pageLayout = new PageLayout();
 function applySquare(){
     const w = document.body.clientWidth;
     const h = document.body.clientHeight;
@@ -9,10 +12,21 @@ function applySquare(){
         perfectSquare.applyHorizontal(w,h);
     }
 }
+function applyLayout(){
+    const w = document.body.clientWidth;
+    const h = document.body.clientHeight;
+    if(h >= w){
+        pageLayout.applyVertical();
+    }else{
+        pageLayout.applyHorizontal();
+    }
+}
 window.addEventListener('resize',()=>{
-    applySquare()
+    applySquare();
+    applyLayout();
 });
-/*set square just at start*/
+/*set square and page layout just at start*/
 window.addEventListener('load',()=>{
-    applySquare()
+    applySquare();
+    applyLayout();
 })
